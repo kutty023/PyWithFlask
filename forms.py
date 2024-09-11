9b99834f64c6aa6f973260c513f2c75c3c709c0e
ARTHI R <arthiaaru23@gmail.com>
2024-09-11 11:04:59 +0000

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class AddTask(FlaskForm):
-    title = StringField('Title', validators= [DataRequired()])
-    submit = SubmitField('Submit')

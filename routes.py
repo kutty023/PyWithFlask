d883d1906bb234c62e3dd68af881acec50c52d2d
ARTHI R <arthiaaru23@gmail.com>
2024-09-11 09:46:14 +0000

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,13 @@
+from app import app 
+from flask import render_template
+
+import forms
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    form = forms.AddTask()
+    return render_template('about.html', form = form)
